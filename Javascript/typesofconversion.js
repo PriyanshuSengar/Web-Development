14fc34d17d85caf6addfe46aaad388f9d4e69c87
Priyanshu Sengar <priyanshusengarps@gmail.com>
2024-02-02 20:50:45 +0530

    Types of Conversion and  Truthy and Falsy Values

---
+++ Javascript/typesofconversion.js
@@ -0,0 +1,16 @@
+// to number conversion
+// to string conversion
+// type coercion
+const newyear = '1515';
+console.log(Number(newyear), newyear);
+console.log(newyear + 14);
+console.log(Number(newyear) + 14);
+console.log(String(25), 25);
+console.log('I am' + 23 + 'year old');
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+let n = '1' + 1;
+n = n - 1;
+console.log(n);
+let m=1+5+3+'5';
+console.log(m);
