ca560bb2345d8c53f1cccecfd2faa25bf80bb7ef
Priyanshu Sengar <priyanshusengarps@gmail.com>
2024-02-06 23:27:39 +0530

    Strict Mode in JS

---
+++ Javascript/StrictModeinJS.js
@@ -0,0 +1,10 @@
+'use strict';
+let hasdriverlicense = false;
+const passtest = true;
+if (passtest) {
+    hasdriverlicense = true;
+    console.log('I can drive');
+}
+// const interface = 'audio';
+// const private = 534;
+// const if= '55';
