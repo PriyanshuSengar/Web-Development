bb2c34797a73e7ced6e9d22a16c5a208b5f0d67f
Priyanshu Sengar <priyanshusengarps@gmail.com>
2024-02-16 22:34:45 +0530

    Looping Array,Breaking.

---
+++ Javascript/Looping,Breaking.js
@@ -0,0 +1,16 @@
+const luckey = [
+    'luckey',
+    'sengar',
+    2037 - 2002,
+    'techer',
+    ['sengar', 'luckey']
+]
+for (let i = 0; i <= 4; i++) {
+    console.log(luckey[i], typeof luckey[i]);
+}
+const year = [1991, 1852, 1963, 2002];
+const age = [];
+for (let i = 0; i < year.length; i++) {
+    age.push(2024 - year[i]);
+}
+console.log(age);
