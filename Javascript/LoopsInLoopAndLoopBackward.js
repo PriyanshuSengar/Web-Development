108df5649aa8ae533cc769c333a460660b0d3afe
Priyanshu Sengar <priyanshusengarps@gmail.com>
2024-02-16 23:15:10 +0530

    Loop in Loop and Backwords

---
+++ Javascript/LoopsInLoopAndLoopBackward.js
@@ -0,0 +1,17 @@
+const luckey = [
+    'luckey',
+    'sengar',
+    2037 - 2002,
+    'techer',
+    ['sengar', 'luckey']
+]
+for (let i = luckey.length - 1; i >= 0; i--) {
+    console.log(luckey[i]);
+
+}
+for (let i = 1; i < 4; i++) {
+    console.log('luckey sengar');
+    for (let j = 1; j < 6; j++) {
+        console.log('good boy');
+    }
+}
